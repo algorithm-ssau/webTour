dadc297941ba4f59af5525ae38924d3f853b78fc
SlavaPavlenko <42034002+SlavaPavlenko@users.noreply.github.com>
2020-04-20 21:37:37 +0300

    Эрмитаж
    Страница Эрмитажа

---
+++ Client/dev/Эрмитаж/gallery.js
@@ -0,0 +1,22 @@
+var images = ["Эрмитаж1.jpg","Эрмитаж2.jpg","Эрмитаж3.jpg"];
+var slideIndex = 0;
+const leftArrow = document.querySelector("left");
+const rightArrow = document.querySelector("right");
+var obj = document.getElementById("gallery_img");
+//const slides = document.querySelectorAll(".slide");
+//leftArrow.addEventListener("click",()=>showSlides(slideIndex-=1));
+//rightArrow.addEventListener("click",()=>showSlides(slideIndex+=1));
+function showSlides(n){
+    if(n>images.length-1) slideIndex = 0;
+    if(n<0) slideIndex=images.length-1;
+    //obj.src = images[slideIndex];
+    //images.forEach((index)=>images[index].style.display="none");
+    //images[slideIndex-1].style.display="flex";
+    loadSlide();
+}
+function loadSlide(){
+    var obj = document.getElementById("gallery_img");
+    obj.src = "images\\"+images[slideIndex];
+    images.forEach((index)=>images[index].style.display="none");
+    images[slideIndex].style.display="block";
+}
