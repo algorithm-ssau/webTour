76d442d7718b845966753d2b05fcd387f14be3ae
Waflya <43246501+Waflya@users.noreply.github.com>
2020-04-24 00:34:11 +0300

    player

--- Client/dev/Эрмитаж/gallery.js
+++ Client/dev/Эрмитаж/gallery.js
@@ -1,22 +1,29 @@
-var images = ["Эрмитаж1.jpg","Эрмитаж2.jpg","Эрмитаж3.jpg"];
+var images = ["Эрмитаж1.jpg", "Эрмитаж2.jpg", "Эрмитаж3.jpg"];
 var slideIndex = 0;
 const leftArrow = document.querySelector("left");
 const rightArrow = document.querySelector("right");
-var obj = document.getElementById("gallery_img");
-//const slides = document.querySelectorAll(".slide");
-//leftArrow.addEventListener("click",()=>showSlides(slideIndex-=1));
-//rightArrow.addEventListener("click",()=>showSlides(slideIndex+=1));
+var iframe;
+iframe.src = "https://www.youtube.com/embed/YuM16V68Pps?enablejsapi=1";
+
 function showSlides(n){
     if(n>images.length-1) slideIndex = 0;
-    if(n<0) slideIndex=images.length-1;
-    //obj.src = images[slideIndex];
-    //images.forEach((index)=>images[index].style.display="none");
-    //images[slideIndex-1].style.display="flex";
-    loadSlide();
+    if (n < 0) slideIndex = images.length;
+    if (n == images.length-1) loadSlidev();
+   
+    else loadSlide();
 }
-function loadSlide(){
+function loadSlide() {
+    iframe = document.querySelector("#ervideo");
+    
+    iframe.style.display = "none"
+
     var obj = document.getElementById("gallery_img");
     obj.src = "images\\"+images[slideIndex];
     images.forEach((index)=>images[index].style.display="none");
     images[slideIndex].style.display="block";
+}
+function loadSlidev() {
+    iframe = document.querySelector("#ervideo");
+    iframe.style.display = "block";
+    
 }
