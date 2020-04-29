c69f65f2c9f039bfb8a83e5ffe0009dfcc4caefe
Waflya <43246501+Waflya@users.noreply.github.com>
2020-04-29 17:21:25 +0400

    updplayer

--- Client/dev/Эрмитаж/gallery.js
+++ Client/dev/Эрмитаж/gallery.js
@@ -1,26 +1,30 @@
-var images = ["Эрмитаж1.jpg", "Эрмитаж2.jpg", "Эрмитаж3.jpg"];
+var images = ["Эрмитаж1.jpg", "Эрмитаж2.jpg", "Эрмитаж3.jpg","video"];
 var slideIndex = 0;
 const leftArrow = document.querySelector("left");
 const rightArrow = document.querySelector("right");
 var iframe;
-iframe.src = "https://www.youtube.com/embed/YuM16V68Pps?enablejsapi=1";
 
 function showSlides(n){
     if(n>images.length-1) slideIndex = 0;
-    if (n < 0) slideIndex = images.length;
-    if (n == images.length-1) loadSlidev();
-   
-    else loadSlide();
+    if (n < 0) slideIndex = images.length-1;
+    loadSlide();
 }
 function loadSlide() {
-    iframe = document.querySelector("#ervideo");
-    
-    iframe.style.display = "none"
+    if (slideIndex == images.length - 1) {
+        loadSlidev();
+    }
+    else {
+        iframe = document.querySelector("#ervideo");
 
-    var obj = document.getElementById("gallery_img");
-    obj.src = "images\\"+images[slideIndex];
-    images.forEach((index)=>images[index].style.display="none");
-    images[slideIndex].style.display="block";
+        iframe.style.display = "none"
+
+        var obj = document.getElementById("gallery_img");
+        obj.src = "images\\" + images[slideIndex];
+        for (var i = 0; i < images.length - 1; i++) {
+            images[i].style.display = "none";
+        }
+        images[slideIndex].style.display = "block";
+    }
 }
 function loadSlidev() {
     iframe = document.querySelector("#ervideo");
