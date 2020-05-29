88c78aeb9dd0caa85ab9ea58b774f202ffc43981
yanyanyanyanyanyanyan <sunny.lapko@gmail.com>
2020-05-29 14:15:53 +0400

    Revert "Revert "Merge branch 'master' of https://github.com/algorithm-ssau/webTour""
    This reverts commit e277a7d8438ed7189690e581478152f685df3314.

---
+++ WebTourWebAPI/WebTour.DAL/Migrations/20200528221348_Category_Names.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebTour.DAL.Migrations
+{
+    public partial class Category_Names : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
